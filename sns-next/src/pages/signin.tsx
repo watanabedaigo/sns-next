c8f7f2a72ba74707aa4ead4435800cacc869f660
渡邊代悟 <wdaigm0801@gmail.com>
2023-01-11 18:53:41 +0900

    ログイン機能実装

--- sns-next/src/pages/signin.tsx
+++ sns-next/src/pages/signin.tsx
@@ -1,10 +1,52 @@
 import type { NextPage } from 'next'
+import type { EventType } from 'types/EventType'
 import Link from 'next/link'
+import { auth } from '../auth/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'next/router'
 
 const SignIn: NextPage = () => {
+  const router = useRouter()
+
+  const handleSubmit = async (e: EventType) => {
+    e.preventDefault()
+
+    const emailInput = e.currentTarget.querySelector(
+      'input[name="email"]'
+    ) as HTMLInputElement
+    const emailValue = emailInput.value
+    console.log(emailValue)
+
+    const passwordInput = e.currentTarget.querySelector(
+      'input[name="password"]'
+    ) as HTMLInputElement
+    const passwordValue = passwordInput.value
+    console.log(passwordValue)
+
+    await signInWithEmailAndPassword(auth, emailValue, passwordValue)
+
+    router.push('/')
+  }
+
   return (
     <div>
       <p>SignIn</p>
+      <div>
+        <h1>ログイン</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">メールアドレス</label>
+            <input id="email" name="email" type="email" placeholder="email" />
+          </div>
+          <div>
+            <label htmlFor="password">パスワード</label>
+            <input id="password" name="password" type="password" />
+          </div>
+          <div>
+            <button>登録</button>
+          </div>
+        </form>
+      </div>
       <ul>
         <li>
           <Link href="/">Home</Link>
