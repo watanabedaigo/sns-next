872b17942022f392c7b58cd2db70d13306e0c39f
渡邊代悟 <wdaigm0801@gmail.com>
2023-01-17 11:44:36 +0900

    アクセスコントロール削除 /「全ての」json-server側の全ユーザーを扱うState追加 / useEffect内json-serverからユーザーデータ取得の処理追加

--- sns-next/src/contexts/AuthContext.tsx
+++ sns-next/src/contexts/AuthContext.tsx
@@ -6,13 +6,16 @@ import {
   useEffect,
 } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
-import { useRouter } from 'next/router'
-import { auth } from '../auth/firebase'
-import type { UserType } from 'types/UserType'
+import { auth } from 'auth/firebase'
+import { FirebaseUserType } from 'types/firebaseUserType'
+import type { JsonUserType } from 'types/JsonUserType'
+import { getData } from 'apis/sns'
 
 // 型エイリアス
 export type AuthContextProps = {
-  firebaseUser: UserType
+  firebaseUser: FirebaseUserType
+  jsonUsers: JsonUserType[]
+  setJsonUsers: any
 }
 
 export type AuthProviderProps = {
@@ -29,38 +32,60 @@ export const useAuthContext = () => {
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  // routerオブジェクト作成
-  const router = useRouter()
-
   // State
-  // ログインしているユーザーを扱う
-  const [firebaseUser, setFirebaseUser] = useState<UserType>(null)
-
-  // ログインしていないユーザーが見ることのできるページを変数で管理
-  const pageForNotLogin =
-    router.pathname === '/signin' || router.pathname === '/signup'
+  // 「ログインしている」firebase側のユーザーを扱う
+  const [firebaseUser, setFirebaseUser] = useState<FirebaseUserType>(null)
+  // 「全ての」json-server側の全ユーザーを扱う
+  const [jsonUsers, setJsonUsers] = useState<JsonUserType[]>([])
+  // リロード対策用のフラッグ
+  const [loading, setLoading] = useState(true)
 
   // contextで管理する値を定義
   const value = {
     firebaseUser,
+    jsonUsers,
+    setJsonUsers,
   }
 
+  // APIリクエスト先のURL
+  const usersUrl = 'http://localhost:3001/users'
+
   // useEffect
+  // 依存配列は空なので、中の処理は初回レンダリング時のみ実行
   useEffect(() => {
+    // クリーンアップ関数（React v18対応）
+    let unmounted = false
+
+    // Read（Users）
+    getData(usersUrl)
+      .then((usersData) => {
+        if (!unmounted) {
+          // State更新
+          // usersDataはallDataType（JsonUserType[] | PostType[]）なので、型アサーションでJsonUserType[]で上書き
+          setJsonUsers(usersData as JsonUserType[])
+        }
+      })
+      .catch((Error) => {
+        console.error(Error)
+      })
+
     // firebaseで用意されている、ログイン情報が変わると発火するonAuthStateChanged
     const authStateChanged = onAuthStateChanged(auth, async (user) => {
       // State更新
       setFirebaseUser(user)
-
-      // ユーザーがnullまたはundefinedでなく（ログインしている）
-      // アクセスしているページのURLがpageForNotLoginでない時（ログイン後にしか見ることのできないページ）
-      // /signupにリダイレクト
-      !firebaseUser && !pageForNotLogin && (await router.push('/signup'))
+      setLoading(false)
     })
+
     return () => {
       authStateChanged()
+      unmounted = true
     }
   }, [])
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={value}>
+      {/* loadingがfalse、つまりonAuthStateChangedが発火してフラッグが変わるのを待ってから表示させる */}
+      {!loading && children}
+    </AuthContext.Provider>
+  )
 }
