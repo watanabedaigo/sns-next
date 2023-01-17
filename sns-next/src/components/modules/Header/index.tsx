c4e09cb0ec3f1cbfd07c6c3f373a522c2dc15c0d
渡邊代悟 <wdaigm0801@gmail.com>
2023-01-17 11:42:04 +0900

    Header 修正

--- sns-next/src/components/modules/Header/index.tsx
+++ sns-next/src/components/modules/Header/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { useAuthContext } from 'contexts/AuthContext'
+import { auth } from 'auth/firebase'
+import { signOut } from 'firebase/auth'
+import { useRouter } from 'next/router'
 
 // メモ化して。親コンポーネントレンダリングによる再レンダリング防止
 const Header: React.FC = React.memo(() => {
+  // routerオブジェクト作成
+  const router = useRouter()
+
   // contextで管理している値を取得
   const { firebaseUser } = useAuthContext()
+
+  // ログアウトの関数を定義
+  const logout = async () => {
+    // firebaseで用意されている、ログアウトの関数
+    await signOut(auth)
+
+    // /signupにリダイレクト
+    await router.push('/signin')
+  }
 
   return (
     <header>
       <p>{firebaseUser ? firebaseUser.email : 'ログイン前'}</p>
-      {firebaseUser && <p>{firebaseUser.uid}</p>}
+      {firebaseUser && <button onClick={logout}>ログアウト</button>}
     </header>
   )
 })
